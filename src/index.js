92a46399001656026626e11cb62771a9cc8b6341
Asis <47801367+Asis2019@users.noreply.github.com>
2023-06-20 10:32:00 +0300

    Removed console log statements and formatting.

--- src/index.js
+++ src/index.js
@@ -13,93 +13,90 @@ import {requestPool} from './requestPool';
 // }
 
 logger.setLogger(console.log);
-const { createFFmpeg } = FFmpeg;
+const {createFFmpeg} = FFmpeg;
 
 const merge2mp4 = async (url, options) => {
-  const { maxLimit = 6, logOpen = true, retryTimes = 3 } = options || {};
-  if (!logOpen) {
-    logger.setLogger(undefined);
-  }
-  const ffmpeg = createFFmpeg();
-  logger.log(`Starting download of ${url}`);
-  const parseObj = await parseM3u8(url);
-  if (!parseObj) {
-    logger.log(`analyze m3u8 list error！`);
-    return null;
-  }
-  console.log(parseObj);
-  debugger
-  let tsArr = parseObj.tsArr;
+    const {maxLimit = 6, logOpen = true, retryTimes = 3} = options || {};
+    if (!logOpen) {
+        logger.setLogger(undefined);
+    }
+    const ffmpeg = createFFmpeg();
+    logger.log(`Starting download of ${url}`);
+    const parseObj = await parseM3u8(url);
+    if (!parseObj) {
+        logger.log(`analyze m3u8 list error！`);
+        return null;
+    }
+    debugger
+    let tsArr = parseObj.tsArr;
 
-  logger.log(`loading ffmpeg...`);
-  await ffmpeg.load();
-  ffmpeg.FS('writeFile', 'index.m3u8', parseObj['index.m3u8']);
-  ffmpeg.setLogging(logOpen);
-  ffmpeg.setProgress(({ ratio }) => {
-    logger.log(`Merge progress ${ratio}.`);
-  });
-
-  const downLoadResult = {
-    successItems: [],
-    errorItems: [],
-    totalItems: tsArr,
-  };
-
-  for (let i = 0; i < retryTimes; i++) {
-    await requestPool({
-      data: tsArr,
-      maxLimit,
-      iteratee: async ({ item }) => {
-        try {
-          ffmpeg.FS(
-            'writeFile',
-            item.fileName,
-            await retryFetchFile(item.path),
-          );
-          // const index = tsArr.indexOf(item);
-          downLoadResult.successItems.push(item);
-          const successCount = downLoadResult.successItems.length;
-          const errorCount = downLoadResult.errorItems.length;
-          const totalCount = downLoadResult.totalItems.length;
-
-          logger.log(
-            `Downloading ts fragments: success ${successCount} part，fail ${errorCount} part，total ${totalCount} part`,
-          );
-        } catch (error) {
-          logger.log(error);
-          downLoadResult.errorItems.push(item);
-        }
-      },
+    logger.log(`loading ffmpeg...`);
+    await ffmpeg.load();
+    ffmpeg.FS('writeFile', 'index.m3u8', parseObj['index.m3u8']);
+    ffmpeg.setLogging(logOpen);
+    ffmpeg.setProgress(({ratio}) => {
+        logger.log(`Merge progress ${ratio}.`);
     });
 
-    console.log(downLoadResult);
+    const downLoadResult = {
+        successItems: [],
+        errorItems: [],
+        totalItems: tsArr,
+    };
 
-    if (
-      downLoadResult.successItems.length === downLoadResult.totalItems.length
-    ) {
-      break;
-    } else {
-      tsArr = downLoadResult.errorItems;
-      downLoadResult.errorItems = [];
+    for (let i = 0; i < retryTimes; i++) {
+        await requestPool({
+            data: tsArr,
+            maxLimit,
+            iteratee: async ({item}) => {
+                try {
+                    ffmpeg.FS(
+                        'writeFile',
+                        item.fileName,
+                        await retryFetchFile(item.path),
+                    );
+                    // const index = tsArr.indexOf(item);
+                    downLoadResult.successItems.push(item);
+                    const successCount = downLoadResult.successItems.length;
+                    const errorCount = downLoadResult.errorItems.length;
+                    const totalCount = downLoadResult.totalItems.length;
+
+                    logger.log(
+                        `Downloading ts fragments: success ${successCount} part，fail ${errorCount} part，total ${totalCount} part`,
+                    );
+                } catch (error) {
+                    logger.log(error);
+                    downLoadResult.errorItems.push(item);
+                }
+            },
+        });
+
+        if (
+            downLoadResult.successItems.length === downLoadResult.totalItems.length
+        ) {
+            break;
+        } else {
+            tsArr = downLoadResult.errorItems;
+            downLoadResult.errorItems = [];
+        }
     }
-  }
 
-  // for (const ts of tsArr) {
-  //   ffmpeg.FS('writeFile', ts.fileName, await retryFetchFile(ts.path));
-  //   const index = tsArr.indexOf(ts);
-  //   logger.log(`正在下载ts片段: 第${index}段，共${tsArr.length}段`);
-  // }
-  logger.log(`Starting ts merge.`);
-  await ffmpeg.run(
-    '-allowed_extensions',
-    'ALL',
-    '-i',
-    'index.m3u8',
-    '-c',
-    'copy',
-    'output.mp4',
-  );
-  return ffmpeg.FS('readFile', 'output.mp4');
+    // for (const ts of tsArr) {
+    //   ffmpeg.FS('writeFile', ts.fileName, await retryFetchFile(ts.path));
+    //   const index = tsArr.indexOf(ts);
+    //   logger.log(`正在下载ts片段: 第${index}段，共${tsArr.length}段`);
+    // }
+    logger.log(`Starting ts merge.`);
+    await ffmpeg.run(
+        '-allowed_extensions',
+        'ALL',
+        '-i',
+        'index.m3u8',
+        '-c',
+        'copy',
+        'output.mp4',
+    );
+    return ffmpeg.FS('readFile', 'output.mp4');
 };
 
 export const setLogger = logger.setLogger;
